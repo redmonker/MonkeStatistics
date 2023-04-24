2005124bc9a0d58046f9185b3a95ddef3570db43
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-04-24 19:17:20 -0400

    MaxJumpSpeed change

--- SpeedBoost/SpeedBoost/Patches/PlayerPatch.cs
+++ SpeedBoost/SpeedBoost/Patches/PlayerPatch.cs
@@ -6,11 +6,22 @@ namespace SpeedBoost.Patches
     [HarmonyPatch(typeof(Player), "FixedUpdate", MethodType.Normal)]
     internal class PlayerPatch
     {
-        [HarmonyPostfix]
-        private static void Postfix()
+        [HarmonyPrefix]
+        private static void Patcj()
         {
             if (Main.RoomValid && Main.Enabled)
+            {
                 Player.Instance.jumpMultiplier = Main.SpeedBoost;
+                Player.Instance.maxJumpSpeed = Main.SpeedBoost;
+            }
+            else
+                if (Player.Instance.maxJumpSpeed = Main.SpeedBoost)
+        }
+
+        // Not a patch, but.... uuu where else do I put it o-o
+        private static void Reset()
+        {
+            Player.Instance.maxJumpSpeed = 6.5f;
         }
     }
 }
