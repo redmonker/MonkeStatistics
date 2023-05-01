20d4662360332c046facbdd2af6217b4dcaf8f73
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-05-01 11:25:12 -0400

    Changed modded room error text

--- MonkeStatistics/MonkeStatistics/Core/Pages/ModdedRoomPage.cs
+++ MonkeStatistics/MonkeStatistics/Core/Pages/ModdedRoomPage.cs
@@ -10,8 +10,8 @@ namespace MonkeStatistics.Core.Pages
             AddText("This page only works in MODDED ROOMS. Please only use it in modded rooms.");
             AddLine(2);
             AddText("Press the back button to return to the main menu.");
-            SetTitle("INVALID ROOM");
-            SetAuthor("ERROR");
+            SetTitle("Modded Gamemode");
+            SetAuthor("Invalid room ERROR");
 
             SetBackButtonOverride(typeof(Pages.MainPage));
             SetLines();
